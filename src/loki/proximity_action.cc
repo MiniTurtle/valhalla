51395534e24b405b8ab6f83134da275095bd28c9
MiniTurtle <miramanagames@gmail.com>
2026-09-03 17:37:38 +0300

    fix segmentation fault

--- src/loki/proximity_action.cc
+++ src/loki/proximity_action.cc
@@ -38,21 +38,6 @@ void loki_worker_t::init_proximity(Api& request) {
     l.clear_heading();
   }
 
-  // check that the number of contours is ok
-  //if (options.contours_size() < 1) {
-  //  throw valhalla_exception_t{113};
-  //} else if (options.contours_size() > max_contours) {
-  //  throw valhalla_exception_t{152, std::to_string(max_contours)};
-  //}
-
-  //// check the contour metrics
-  //for (auto& contour : options.contours()) {
-  //  if (contour.has_time_case() && contour.time() > max_contour_min)
-  //    throw valhalla_exception_t{151, std::to_string(max_contour_min)};
-  //  if (contour.has_distance_case() && contour.distance() > max_contour_km)
-  //    throw valhalla_exception_t{166, std::to_string(max_contour_km)};
-  //}
-
   parse_costing(request);
 }
 
@@ -67,15 +52,19 @@ void loki_worker_t::proximity(Api& request) {
   if (options.locations_size() < 2) {
     throw valhalla_exception_t{150, "Requires at least 2 locations"};
   };
-  if ((size_t)options.locations_size() > max_locations.find("proximity")->second) {
-    throw valhalla_exception_t{150, std::to_string(max_locations.find("proximity")->second)};
+  auto max_loc_it = max_locations.find("proximity");
+  int max_locs = max_loc_it != max_locations.end() ? max_loc_it->second : 400; // default to 20 if missing
+  if (options.locations_size() > max_locs) {
+    throw valhalla_exception_t{150, std::to_string(max_locs)};
   };
 
   if (options.max_search_locations() < 1)
       throw valhalla_exception_t{150, "Requires at least 1 max_search_locations"}; 
 
   // check the distances
-  check_distance(options.locations(), max_distance.find("proximity")->second);
+  auto max_dist_it = max_distance.find("proximity");
+  float max_dist = max_dist_it != max_distance.end() ? max_dist_it->second : 100000.0f; // default to 100km if missing
+  check_distance(options.locations(), max_dist);
 
   try {
     // correlate the various locations to the underlying graph
